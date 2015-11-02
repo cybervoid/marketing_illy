c209ebff523457bdc2753b940caf1d4398b34192
Rafael Gil <rafaelgil83@gmail.com>
2015-11-02 11:53:25 -0500

    master branch for live server, unfinished links disabled.

---
+++ public/js/admin.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rafag on 3/21/15.
+ */
+
+var App = App || {};
+
+(function ($) {
+    "use strict";
+
+    App.newHire = App.newHire || {};
+
+
+    $(document).ready(function () {
+        $('#users').submit(function () {
+            alert('This option is under development');
+            return false;
+        });
+
+        $('#admin').submit(function () {
+            alert('This option is under development');
+            return false;
+        });
+
+
+        $('#upload_frm').submit(function () {
+            alert('This option is under development');
+            return false;
+        });
+
+
+    });
+
+}(jQuery));
