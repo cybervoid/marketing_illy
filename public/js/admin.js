ceb13ca7ae64aff853fe22409b880f84e2560934
Rafael Gil <rafaelgil83@gmail.com>
2015-11-10 19:33:14 -0500

    gulp added

--- public/js/admin.js
+++ public/js/admin.js
@@ -11,20 +11,23 @@ var App = App || {};
 
 
     $(document).ready(function () {
+
         $('#users').submit(function () {
-            alert('This option is under development');
-            return false;
+
+            //alert('This option is under development');
+            if($('#user_new_pass').val()!=$('#user_confirm_pass').val()){
+                $('#user_frm_error').html("New passwords doesn't match.");
+                return false;
+            }
+
         });
 
         $('#admin').submit(function () {
-            alert('This option is under development');
-            return false;
-        });
+            if($('#adm_new_pass').val()!=$('#adm_confirm_pass').val()){
+                $('#adm_frm_error').html("New passwords doesn't match.");
+                return false;
+            }
 
-
-        $('#upload_frm').submit(function () {
-            alert('This option is under development');
-            return false;
         });
 
 
